8a09fc23f3667fde7dffaa5b3c7ed07a66dce3ee
gustavo de paula zampieri <128715598+NINJAPRECOCE@users.noreply.github.com>
2024-05-28 08:35:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-
+const caixaPrincipal = document.queryselector("caixa-principal")
+const caixaPerguntas = document.querySelector("caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-resultado")
+const caixaResultado = document.querySelector (".caixa-resultado");
+const textoResultado = document.querySelector =(".texto-resultado');
+                                                
+const perguntas = [
+  {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+   {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+   {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+   {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+   {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+   {
+    enunciado:"Pergunta1"
+    alternativas:[
+  "alternativas",
+  "alternativas 2",
+  }  
+]
